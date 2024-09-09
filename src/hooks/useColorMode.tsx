00109723d54773e990ab6a4bbd9f53fa714eabf0
Ahmed-mohamed11 <ahmedmohamed0aa349@gmail.com>
2024-09-09 17:00:29 +0300

    commit

--- src/hooks/useColorMode.tsx
+++ src/hooks/useColorMode.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
@@ -7,16 +7,17 @@ const useColorMode = () => {
 
   useEffect(() => {
     if (typeof window !== "undefined") {
+      const className = "dark";
+      const bodyClass = window.document.documentElement.classList;
 
-    const className = "dark";
-    const bodyClass = window.document.documentElement.classList;
+      // Update the class list based on colorMode
+      colorMode === "dark"
+        ? bodyClass.add(className)
+        : bodyClass.remove(className);
+    }
+  }, [colorMode]); 
 
-    colorMode === "dark"
-      ? bodyClass.add(className)
-      : bodyClass.remove(className);
-  }  }, [colorMode]);
-
-  return [colorMode, setColorMode];
+  return [colorMode, setColorMode] as const;
 };
 
 export default useColorMode;
